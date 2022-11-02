729a8a985604e37e5a3d77fd6a3a77b0d70d3dcd
Souradip Adhikari <108760780+SouradipAdhikari03@users.noreply.github.com>
2022-11-02 23:46:13 +0530

    Update game.js
    Worked More on js File

--- game.js
+++ game.js
@@ -1,33 +1,143 @@
-*{
-    margin:0;
-    padding:0;
+const score=document.querySelector(".score");
+const startScreen=document.querySelector(".startScreen");
+const gameArea=document.querySelector(".gameArea");
+let carsimg=['car2','car3','car4','car5','car6','car7']
+player={speed:5, score:0};
+const gameplay=()=>{
+
+    // console.log("clicked");
+    let car=document.querySelector(".car");
+    const road=gameArea.getBoundingClientRect();
+    if (player.start) {
+        moveLines();
+        moveEnemy(car);
+        if (keys.ArrowUp && player.y>60) {
+            player.y-=player.speed;
+            
+        }
+        if (keys.ArrowDown && player.y<road.bottom-75) {
+            player.y+=player.speed;
+            
+        }
+        if (keys.ArrowLeft && player.x>0) {
+            player.x-=player.speed;
+            
+        }
+        if (keys.ArrowRight && player.x<(road.width-40)) {
+            player.x+=player.speed;
+            
+        }
+        car.style.left=player.x+"px"
+        car.style.top=player.y+"px"
+        window.requestAnimationFrame(gameplay)
+        score.innerText="score :"+player.score;
+        player.score++;
+    }
 }
-body{
-    height: 657px;
-    width: 100%;
+const start=()=>{ 
+    player.start=true;
+    gameArea.innerHTML="";
+    startScreen.classList.add("hide")
+    player.score=0;
+    window.requestAnimationFrame(gameplay)
+    
+    
+    for(x=0; x<8;x++){
+        let roadLine=document.createElement("div")
+        roadLine.setAttribute("class","lines")
+        roadLine.y=(x*150);
+        
+        roadLine.style.top=roadLine.y+"px"
+        gameArea.appendChild(roadLine)
+    }
+    
+    let car=document.createElement('div')
+    car.setAttribute('class','car')
+    // car.innerText=""
+    gameArea.appendChild(car)
+    player.x=car.offsetLeft;
+    player.y=car.offsetTop;
+    
+
+    
+    for(x=0; x<4;x++){
+        let enemyCar=document.createElement("div")
+        enemyCar.setAttribute("class","enemy")
+        enemyCar.y=((x+1)*350)*-1;
+        enemyCar.style.top=enemyCar.y+"px"
+        ci="'"+carsimg[Math.floor(Math.random()*6)]+".png'";
+        enemyCar.style.backgroundImage=`url(${ci})`;       
+        enemyCar.style.left=Math.floor(Math.random()*350)+"px"
+        
+        gameArea.appendChild(enemyCar)
+    }
 }
-.hide{
-    display: none;
+startScreen.addEventListener("click",start);
+
+keys={
+    ArrowUp:false,
+    ArrowDown:false,
+    ArrowRight:false,
+    ArrowLeft:false
 }
-.gameArea{
-    height:750px;
-    width: 400px;
-    background-color: gray;
-    margin: auto;
-    position: relative;
+
+
+
+keyDown=(e)=>{
+    e.preventDefault();
+    keys[e.key]=true;
+    // console.log(e.key);
+    // console.log(keys);
 }
-.car, .enemy{
-    position: absolute;
-    height: 40px;
-    width: 40px;
-    bottom: 120px;
-    background-color: brown;
-    z-index: 1;
+
+function keyUp(e){
+    e.preventDefault();
+    keys[e.key]=false;
+    // console.log(e.key);
+    // console.log(keys);
 }
-.lines{
-    width: 10px;
-    height: 100px;
-    background: white;
-    position: absolute;
-    margin-left: 195px;;
+document.addEventListener("keydown",keyDown);
+document.addEventListener("keyup",keyUp);
+
+
+moveLines=()=>{
+    let lines=document.querySelectorAll(".lines")
+    lines.forEach((item)=>{
+        if (item.y>=700) {
+            item.y-=750
+        }
+        item.y+=player.speed;
+        item.style.top=item.y+"px"
+    }) 
+}
+
+
+
+isCollide=(a,b)=>{
+    aRect=a.getBoundingClientRect();
+    bRect=b.getBoundingClientRect();
+
+    return !((aRect.bottom<bRect.top) || (aRect.top>bRect.bottom) || (aRect.right<bRect.left) || (aRect.left>bRect.right))
+}
+endGame=()=>{
+    player.start=false;
+    startScreen.classList.remove("hide")
+    // gameArea.classList.add("hide")
+    startScreen.innerHTML="GAME OVER!! <br>your score is:"+player.score+ "<br> tap to restart!" ;
+}
+moveEnemy=(car)=>{
+    let enemy=document.querySelectorAll(".enemy")
+    enemy.forEach((item)=>{
+        if(isCollide(car,item)){
+            console.log("boom");
+            endGame();
+            // startScreen.appendChild(message)
+        }
+        if (item.y>=700) {
+            item.y=-300
+            item.style.left=Math.floor(Math.random()*350)+"px"
+        }
+        item.y+=player.speed;
+        item.style.top=item.y+"px"
+    }) 
 }
